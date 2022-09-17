0b93aca84e1e0b6caa9e4b40e17b3880f40b03d4
Tony Nguyen <nguyen.tu.tony2000@gmail.com>
2022-09-18 00:32:08 +0200

    Added Static bool for logs

--- Assets/Scripts/Core/Behaviours/BaseBehaviour.cs
+++ Assets/Scripts/Core/Behaviours/BaseBehaviour.cs
@@ -5,7 +5,8 @@ using Object = UnityEngine.Object;
 
 namespace VHS {
     public class BaseBehaviour : MonoBehaviour, ICustomUpdateListener {
-        [FoldoutGroup("Debug Properties"), SerializeField] private bool _showLogs = true;
+        [FoldoutGroup("Debug Properties"), ShowInInspector] private static bool DISABLE_ALL_LOGS = false;
+        [FoldoutGroup("Debug Properties"), SerializeField, DisableIf("DISABLE_ALL_LOGS")] private bool _instanceLogs = true;
         [FoldoutGroup("Debug Properties"), SerializeField, ColorUsage(false,false)] private Color _color = Color.white;
         
         [FoldoutGroup("Behaviour Properties"),SerializeField] private bool _customUpdate = false;
@@ -18,6 +19,7 @@ namespace VHS {
         private Transform _transform;
         private WaitForEndOfFrame _frame;
         private WaitForFixedUpdate _fixedFrame;
+
 
         protected WaitForEndOfFrame Frame => _frame ??= new WaitForEndOfFrame();
         protected WaitForFixedUpdate FixedFrame => _fixedFrame ??= new WaitForFixedUpdate();
@@ -44,7 +46,7 @@ namespace VHS {
         public virtual void OnCustomUpdate(float deltaTime) { }
 
         protected void Log(params object[] msg) {
-            if(!_showLogs)
+            if(!DISABLE_ALL_LOGS || !_instanceLogs)
                 return;
 
             VHSLogger.DoLog(Debug.Log, GetType().Name , gameObject, _color, msg );
