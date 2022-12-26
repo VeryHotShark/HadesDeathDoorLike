8fd31901fd641985f2f87b9cedc908e1cbd1416e
Tony Nguyen <nguyen.tu.tony2000@gmail.com>
2022-12-26 12:04:06 +0100

    Experimenting Wiith UModeler

--- Assets/Plugins/See-through Shader/Core/STS Shader Customization/STS ShaderGraph/SeeThroughShaderCustomFunctionURPnBiRP.hlsl
+++
@@ -1,6 +0,0 @@
-#ifndef STS_CUSTOM_SHADERGRAPH_FUNCTIONS
-#define STS_CUSTOM_SHADERGRAPH_FUNCTIONS
-
-#include "Assets/See-through Shader/Core/STS Shader Customization/STS ShaderGraph/SeeThroughShaderCustomFunctionBase.hlsl"
-
-#endif
