8a313caaeb0022871e1929862d5d2c84883057b1
VeryHotShark <nguyen.tu.tony2000@gmail.com>
2023-03-12 10:23:18 +0100

    All in 1 Toolkit Import WIP

---
+++ Assets/Scripts/Game/Contraptions/Particles/ParticleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VHS {
+    public class ParticleController : BaseBehaviour, IPoolable{
+        private ParticleSystem _particle;
+        
+        void Awake() => _particle = GetComponentInChildren<ParticleSystem>();
+
+        public void Play() => _particle.Play(true);
+
+        public override void OnCustomUpdate(float deltaTime) {
+            if(!_particle.IsAlive(true))
+                PoolManager.Return(this);
+        }
+    }
+}
--- Assets/Scripts/Game/Skills/Player/PlayerSkillGrenade.cs
+++ Assets/Scripts/Game/Skills/Player/PlayerSkillGrenade.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 namespace VHS {
     [Serializable]
     public class PlayerSkillGrenade : PlayerSkill {
+        public ParticleController _explosionVFX;
+        public ParticleController _indicatorVFX;
+        
+        
         public float _radius = 3.0f;
         public int _damage = 1;
 
@@ -13,9 +17,21 @@ namespace VHS {
         private Collider[] _colliders = new Collider[32];
 
         private Vector3 _castPosition;
+        private ParticleController _indicatorInstance;
 
         public override void OnReset() {
             _hittables.Clear();
+        }
+
+        public override void OnCastStart() {
+            _indicatorInstance = PoolManager.Spawn(_indicatorVFX,
+                Owner.PlayerController.Camera.CursorTransform.position, Quaternion.identity);
+            _indicatorInstance.transform.SetParent(Owner.PlayerController.Camera.CursorTransform);
+            
+        }
+
+        public override void OnCastFinish() {
+            PoolManager.Return(_indicatorInstance);
         }
 
         public override void OnCastTick(float deltaTime) {
@@ -26,8 +42,9 @@ namespace VHS {
         public override void OnSkillFinish() {
             int hitCount =
                 Physics.OverlapSphereNonAlloc(_castPosition, _radius, _colliders, LayerManager.Masks.NPC);
-
-            DebugExtension.DebugWireSphere(_castPosition, Color.red, _radius, 2.0f);
+            
+            ParticleController particleController = PoolManager.Spawn(_explosionVFX, _castPosition, Quaternion.identity);
+            particleController.Play();
             
             if (hitCount == 0)
                 return;
